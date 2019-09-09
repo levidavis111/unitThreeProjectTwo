634bc96dc3aefb8a9bd4e1bb6d8ab50436e591ae
levi davis <levidavis@pursuit.org>
2019-09-09 16:14:05 -0400

    Adds show model

--- unit-three-project-two/Show.swift
+++ unit-three-project-two/Show.swift
@@ -7,3 +7,42 @@
 //
 
 import Foundation
+
+struct Show: Codable {
+    
+    let show: ShowWrapper
+    let image: ImageWrapper
+    
+    
+    struct ShowWrapper: Codable {
+        let id: Int
+        let url: String
+        let name: String
+    }
+    
+    struct ImageWrapper: Codable {
+        let medium: String
+        let original: String
+    }
+    
+    
+    static func getShowData(completionHandler: @escaping (Result<[Show],AppError>) -> () ) {
+        let url = "http://api.tvmaze.com/search/shows?q=girls"
+        
+        NetworkManager.shared.fetchData(urlString: url) { (result) in
+            switch result {
+            case .failure(let error):
+                completionHandler(.failure(error))
+            case .success(let data):
+                do {
+                    let showResults = try JSONDecoder().decode(Show.self, from: data)
+                    
+                    completionHandler(.success([showResults]))
+                } catch {
+                    completionHandler(.failure(.badJSONError))
+                }
+            }
+            
+        }
+    }
+}

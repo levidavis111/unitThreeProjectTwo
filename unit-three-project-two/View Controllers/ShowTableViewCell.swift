d0feb77c60da33ef63717509781b93c095469543
levi davis <levidavis@pursuit.org>
2019-09-09 16:37:38 -0400

    Sets up initial view controller in storyboard.

---
+++ unit-three-project-two/View Controllers/ShowTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ShowTableViewCell.swift
+//  unit-three-project-two
+//
+//  Created by Levi Davis on 9/9/19.
+//  Copyright © 2019 Levi Davis. All rights reserved.
+//
+
+import UIKit
+
+class ShowTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var showImageView: UIImageView!
+    @IBOutlet weak var showNameLabel: UILabel!
+    
+}
---
+++ unit-three-project-two/View Controllers/ShowViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ShowViewController.swift
+//  unit-three-project-two
+//
+//  Created by Levi Davis on 9/9/19.
+//  Copyright © 2019 Levi Davis. All rights reserved.
+//
+
+import UIKit
+
+class ShowViewController: UIViewController {
+
+    @IBOutlet weak var showSearchBar: UISearchBar!
+    @IBOutlet weak var showTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

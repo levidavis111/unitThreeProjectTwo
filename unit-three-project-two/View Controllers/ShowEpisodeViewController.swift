5fd7b7044bc1d15d8ee2d0d918413c76b6e1d615
levi davis <levidavis@pursuit.org>
2019-09-10 15:22:35 -0400

    Calls data from Episode model into episodes vc and sets up cells

--- unit-three-project-two/View Controllers/ShowEpisodeViewController.swift
+++ unit-three-project-two/View Controllers/ShowEpisodeViewController.swift
@@ -11,28 +11,59 @@ import UIKit
 class ShowEpisodeViewController: UIViewController {
     
     var oneShow: Show!
-    var 
+    var episodes: [Episode]? {
+        didSet {
+            episodeTableView.reloadData()
+        }
+    }
 
     @IBOutlet weak var episodeTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         episodeTableView.delegate = self
         episodeTableView.dataSource = self
-
+        loadData()
         // Do any additional setup after loading the view.
     }
 
+    func loadData() {
+        Episode.getEpisodeData(showID: oneShow.show.id) { (result) in
+            DispatchQueue.main.async {
+                switch result {
+                case .failure(let error):
+                    print(error)
+                case .success(let episodes):
+                    self.episodes = episodes
+                }
+            }
+        }
+    }
+    
+    
 }
 
 extension ShowEpisodeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return episodes?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         if let cell = episodeTableView.dequeueReusableCell(withIdentifier: "episodeCell", for: indexPath) as? ShowTableViewCell {
-            cell.episodeNameLabel.text = oneShow.show.name
+            let oneEpisode = episodes?[indexPath.row]
+            ImageHelper.shared.fetchImage(urlString: oneEpisode?.image.original ?? "") { (results) in
+                DispatchQueue.main.async {
+                    switch results {
+                    case .failure(let error):
+                        print(error)
+                    case .success(let image):
+                        cell.episodeImageView.image = image
+                    }
+                }
+            }
+            
+            cell.episodeNameLabel.text = oneEpisode?.name
+            cell.epsiodeSeasonAndNumberLabel.text = "S: \(oneEpisode?.season ?? 0) E: \(oneEpisode?.number ?? 0)"
             return cell
         }
         
@@ -40,7 +71,7 @@ extension ShowEpisodeViewController: UITableViewDelegate, UITableViewDataSource 
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        150
+        return 150
     }
     
     
